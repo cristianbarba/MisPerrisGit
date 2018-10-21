3f395f5c52341f3a17784c71071dc5d297260fc5
Cristian Barba <cri.barba@alumnos.duoc.cl>
2018-10-21 18:23:12 -0300

    Error al agregar usuario, constraint comuna

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home,registro,registroComunas
+from .views import *
 
 
 urlpatterns = [
--- core/views.py
+++ core/views.py
@@ -11,19 +11,76 @@ def registro(request):
     ciudad=Comuna.objects.all()
     vivienda=Tipo_Vivienda.objects.all()
     usuario=Tipo_usuario.objects.all()
-    return render(request, 'core/registro.html',{
-          'genero':genero,
+
+    variables= {
+         'genero':genero,
           'region':region,
           'ciudad':ciudad,
           'vivienda':vivienda,
           'usuario':usuario
-    })
+    }
+
+    if request.POST:
+        usuario= Usuario()
+        usuario.rut_usuario=request.POST.get('txtRun')
+        usuario.nombre=request.POST.get('txtNombre')
+        usuario.ap_pat=request.POST.get('txtApPat')
+        usuario.ap_mat=request.POST.get('txtApMat')
+
+        genero=Genero()
+        genero.id=int(request.POST.get('cboGenero'))
+        usuario.genero=genero
+
+
+
+        usuario.fec_nac=request.POST.get('txtNacimiento')
+        usuario.telefon=request.POST.get('txtfono')
+
+        region=Region()
+        region.id=int(request.POST.get('cboRegion'))
+        usuario.region=region
+
+
+        #error al registrar comuna 
+        ciudad=Comuna()
+        ciudad.id=int(request.POST.get('cboCuidad'))
+        usuario.ciudad=ciudad
+
+
+        usuario.direccion=request.POST.get('txtDireccion')
+        usuario.mail=request.POST.get('txtMail')
+
+
+        tipoVivienda=Tipo_Vivienda()
+        tipoVivienda.id=int(request.POST.get('cboTipoVivienda'))
+        usuario.tipoVivienda=tipoVivienda
+
+
+
+        tipoUsuario=Tipo_usuario()
+        tipoUsuario.id=int(request.POST.get('cboUser'))
+        usuario.tipoUsuario=tipoUsuario
+
+
+        try:
+            usuario.save()
+            variables['mensaje']= "Se registro el usuario"
+        except Exception as a:
+            variables['mensaje']= "Error al registrar Usuario" + str(a)
+            
+
+
+
+
+    return render(request, 'core/registro.html',variables)
 
 def registroComunas(request,id):  #nuevo metodo view que filtra las comunas segun el id rescatado
     ciudad=Comuna.objects.filter(idregion=id) #filtro por idregion (id de clase en models.py)
     return render(request, 'core/comboComuna.html',{ #usa el html combo comuna para escribir los opcion
           'ciudad':ciudad, #retorna tambien la ciudad
    })
+
+
 
 
 
