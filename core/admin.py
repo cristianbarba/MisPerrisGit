a9d4b081bf317a2a7374a702b19ee647dff522df
Cristian Barba <cri.barba@alumnos.duoc.cl>
2018-10-26 10:36:37 -0300

    Panel Administracion django, Listo

--- core/admin.py
+++ core/admin.py
@@ -4,14 +4,25 @@ from core.models import *
 
 
 class MascotaAdmin(admin.ModelAdmin):
-    list_filter = ('esterilizado')
+    list_display=('nombre','esterilizado')
+    search_fields=['nombre']
+    list_filter=('id_raza','cod_refugio')
+
+
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display=('rut_usuario','nombre','ap_pat')
+    search_fields=['rut_usuario']
+    list_filter=('id_comuna','id_genero')
+
+
+
 
 
 admin.site.register(Region)
 admin.site.register(Comuna)
 admin.site.register(Tipo_Vivienda)
 admin.site.register(Tipo_usuario)
-admin.site.register(Usuario)
+admin.site.register(Usuario,UsuarioAdmin)
 admin.site.register(Trabajador)
 admin.site.register(Login)
 admin.site.register(Fondo_Economico)
@@ -21,7 +32,7 @@ admin.site.register(Insumo)
 admin.site.register(Refugio)
 admin.site.register(Raza)
 admin.site.register(Origen_mascota)
-admin.site.register(Mascota)
+admin.site.register(Mascota,MascotaAdmin)
 admin.site.register(Visita_Medica)
 admin.site.register(Tipo_campania)
 admin.site.register(Campania)
